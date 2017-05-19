836dabaa646cc879213dbd9580b1d20d8ff3b4ab
ardwin ponce <ardwin.ivan.a.ponce@accenture.com>
2017-05-19 13:05:05 +0800

    changed

--- test.groovy
+++ test.groovy
@@ -1,9 +1,19 @@
-def generateSampleJob = freeStyleJob("bago")
+def generateSampleJob = freeStyleJob("cart2")
 
 generateSampleJob.with {
+	steps {
+	scm {
+        git {
+            remote {
+                url('https://github.com/johnvinson/sample-repo.git')
+                credentials('269dd22f-8bfb-4266-a53e-575dcc1446c4')
+            }
+        }
+    }
+	maven {
+		mavenInstallation('maven')
+		goals('package')
+	}
+	} 
 	
-		scm{
-	git('https://github.com/ardwin1112/junit-sample.git')
-	}
-	}
-
+}   
